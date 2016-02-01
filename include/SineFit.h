2defd8f21449f83ed63677bad10b6a0907e4be25
Thomas Schmiedel <thomas.schmiedel@tu-ilmenau.de>
2016-02-01 22:10:33 +0100

    wip

--- include/SineFit.h
+++ include/SineFit.h
@@ -49,14 +49,47 @@ private:
     SineFitter &operator=(const SineFitter &other);
 
 public:
-    SineFitter(const uint particles,
+    SineFitter(const uint windowSize,
+               const uint particles,
                const NumericalType W,
                const NumericalType cp,
-               const NumericalType cg) : mParticles(particles),
+               const NumericalType cg) : mWindow(windowSize),
+                                         mParticles(particles),
                                          mW(W),
                                          mCp(cp),
                                          mCg(cg)
     { }
+
+    // compute min max normalization
+    void normalize(const std::vector<NumericalType> &data,
+                   const uint startAt,
+                   NumericalType *minVal,
+                   NumericalType *scaling)
+    {
+        // normalize data window to 0-1
+        NumericalType vmin = (NumericalType)FLT_MAX,
+                      vmax = (NumericalType)-FLT_MAX;
+        for (uint k = 0; k < mWindow; ++k)
+        {
+            const uint idx = startAt + k;
+            if (data[idx] < vmin) vmin = data[idx];
+            if (data[idx] > vmax) vmax = data[idx];
+        }
+        NumericalType scale;
+        if (vmin == vmax)
+        {
+            scale = (NumericalType)1.0;
+            vmin -= (NumericalType)0.5;
+        }
+        else
+        {
+            scale = (NumericalType)1.0 / (vmax - vmin);
+        }
+
+        // commit
+        *minVal = vmin;
+        *scaling = scale;
+    }
 
     // actual pso fitting
     SineParams<NumericalType> fit(const std::vector<NumericalType> &data)
@@ -72,7 +105,8 @@ public:
     }
 
 private:
-    uint mParticles;
+    uint mWindow,
+         mParticles;
     NumericalType mW,
                   mCp,
                   mCg;
